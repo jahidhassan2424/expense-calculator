4e8f939cc2c577bdbcdc1661938f514c782ca15e
jahidhassan2424 <jahidhassan.programmer@gmail.com>
2022-02-17 17:45:58 +0600

    Error Validation Done

--- js/monthly-expense-calculator.js
+++ js/monthly-expense-calculator.js
@@ -1,14 +1,16 @@
-const inputIncome = document.getElementById('input-income');
+// Variable Declearation
+const inputIncome = variable('input-income');
+const inputExpenseFood = variable('inputExpenseFood');
+const inputExpenseRent = variable('inputExpenseRent');
+const inputExpenseCloths = variable('inputExpenseCloths');
+const totalExpense = variable('totalExpense');
+const balance = variable('balance');
+const popupError = variable('popupError');
+const inputExpenseSave = variable('inputExpenseSave');
+const buttonCalculate = variable('button-calculate');
+const buttonSave = variable('buttonSave');
 
-const inputExpenseFood = document.getElementById('inputExpenseFood');
-const inputExpenseRent = document.getElementById('inputExpenseRent');
-const inputExpenseCloths = document.getElementById('inputExpenseCloths');
-const totalExpense = document.getElementById('totalExpense');
-const balance = document.getElementById('balance');
-const popupError = document.getElementById('popupError');
-const inputExpenseSave = document.getElementById('inputExpenseSave');
-const buttonCalculate = document.getElementById('button-calculate');
-const buttonSave = document.getElementById('buttonSave');
+// Calculate Button Functionality
 buttonCalculate.addEventListener('click', function () {
     inputIncome.style.borderColor = 'black';
     inputExpenseFood.style.borderColor = 'black';
@@ -17,46 +19,65 @@ buttonCalculate.addEventListener('click', function () {
     popupError.style.display = 'none';
     if (isNaN(parseInt(inputIncome.value)) || parseInt(inputIncome.value) < 0) {
         console.log('Invalid Input');
-        errorMsg(inputIncome);
+        errorMsg(inputIncome, 'Income must be a positive number');
     }
     else if (isNaN(parseInt(inputExpenseFood.value)) || parseInt(inputExpenseFood.value) < 0) {
         console.log('Invalid Input');
-        errorMsg(inputExpenseFood);
+        errorMsg(inputExpenseFood, 'Food Expenses must be a positive number');
     }
     else if (isNaN(parseInt(inputExpenseRent.value)) || parseInt(inputExpenseRent.value) < 0) {
         console.log('Invalid Input');
-        errorMsg(inputExpenseRent);
+        errorMsg(inputExpenseRent, 'Rent Expenses must be a positive number');
     }
     else if (isNaN(parseInt(inputExpenseCloths.value)) || parseInt(inputExpenseCloths.value) < 0) {
         console.log('Invalid Input');
-        errorMsg(inputExpenseCloths);
+        errorMsg(inputExpenseCloths, 'Cloth Expenses must be a positive number');
     }
     else {
         totalExpense.innerText = (parseInt(inputExpenseFood.value) + parseInt(inputExpenseRent.value) + parseInt(inputExpenseCloths.value))
         balance.innerText = (inputIncome.value - totalExpense.innerText);
-        saveSection();
+        remainingBalance.innerText = parseInt(balance.innerText);
     }
 
-    // console.log(typeof (parseInt(inputIncome)));
-    // console.log(parseInt(inputIncomeValue))
-
 })
+// Saving Section
 buttonSave.addEventListener('click', function () {
-    savingAmount.innerText = (balance.innerText / inputExpenseSave.value);
-    remainingBalance.innerText = balance.innerText - savingAmount.innerText;
+    popupError.style.display = 'none';
+    if (isNaN(parseInt(inputExpenseSave.value)) || parseInt(inputExpenseSave.value) < 0) {
+        console.log('Invalid Input');
+    }
+    else {
+        if ((parseInt(inputIncome.value)) * (parseInt(inputExpenseSave.value) / 100) > parseInt(balance.innerText)) {
+            // console.log('You cant save more than your remaining balance');
+            errorMsg(inputExpenseSave, 'You cant save more than your remaining balance')
+        }
+        else {
+            savingAmount.innerText = (parseInt(inputIncome.value)) * (parseInt(inputExpenseSave.value) / 100);
+            remainingBalance.innerText = substract(balance, savingAmount);
+            inputExpenseSave.style.borderColor = 'black';
+        }
+    }
 })
-function errorMsg(fieldName) {
+//Variable Declearation Function
+function variable(idName) {
+    return document.getElementById(idName);
+
+}
+
+
+// Show Error Aleart Function
+function errorMsg(fieldName, text) {
     fieldName.style.borderColor = 'red';
     popupError.style.display = 'block';
-    popupErrorInnerText.innerText = 'Input must be positive number';
-}
-function saveSection() {
-    savingAmount.innerText = (balance.innerText / inputExpenseSave.value);
-    remainingBalance.innerText = balance.innerText - savingAmount.innerText;
+    popupErrorInnerText.innerText = text;
 }
 
+// substract Function
+function substract(num1, num2) {
+    return parseInt(num1.innerText) - parseInt(num2.innerText);
+}
+
+// Close popup Error
 popupError.addEventListener('click', function () {
     popupError.style.display = 'none';
-
-
 })
